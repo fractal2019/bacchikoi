e1aa28cdefa34d32ef91ace597ed7d816a0ad4b2
kei <kotoura5545@gmail.com>
2019-02-20 03:39:51 +0000

    g controller&view admins&admins_users&admins_products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'admins_users/index'
+  get 'admins_users/show'
+  get 'admins_users/edit'
+  get 'admins_products/index'
+  get 'admins_products/new'
+  get 'admins_products/show'
+  get 'admins_products/edit'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
