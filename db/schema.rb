94bd818b4a92fe3bb11412c9ccb4f3535560707e
Takagi <uruhuodoneru2002@gmail.com>
2019-02-20 10:20:33 +0000

    db追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,65 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_083351) do
+ActiveRecord::Schema.define(version: 2019_02_20_100131) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "artist_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "discs", force: :cascade do |t|
+    t.string "disc_name"
+    t.integer "product_id"
+    t.integer "disc_order"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "jackets", force: :cascade do |t|
+    t.string "jacket_image_id"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "labels", force: :cascade do |t|
+    t.integer "label_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.integer "type_name"
+    t.integer "price"
+    t.integer "number_of_stock"
+    t.integer "lebel_id"
+    t.datetime "deleted_at"
+    t.text "product_introduction"
+    t.text "staff_coment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "purchase_histories", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "destination_postal_code"
+    t.text "destination_address"
+    t.integer "delivery_status"
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "song_name"
+    t.integer "disc_id"
+    t.integer "genre_name"
+    t.integer "track_order"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -18,6 +76,12 @@ ActiveRecord::Schema.define(version: 2019_02_19_083351) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
