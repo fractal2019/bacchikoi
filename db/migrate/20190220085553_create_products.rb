f6e45e21885021336b38348b9080ad1f76d5aaa4
Takagi <uruhuodoneru2002@gmail.com>
2019-02-20 09:41:07 +0000

    model追加

---
+++ db/migrate/20190220085553_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.integer :type_name
+      t.integer :price
+      t.integer :number_of_stock
+      t.integer :lebel_id
+      t.datetime :deleted_at
+      t.text :product_introduction
+      t.text :staff_coment
+
+      t.timestamps
+    end
+  end
+end
