1e83e900609c542e2ba528eed89ccda3dbff7b4b
kei <kotoura5545@gmail.com>
2019-02-21 06:19:18 +0000

    fix migrate

--- db/migrate/20190221045645_add_columns_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnsToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :deleted_at, :datetime
-    add_column :users, :tel, :integer
-    add_column :users, :postal_code, :string
-    add_column :users, :address, :string
-    add_column :users, :name_kana, :string
-  end
-end
