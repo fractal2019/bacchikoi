9851a812a1bf715bb87091e02b41db7a5ef09df1
kohei15 <101j00113@gmail.com>
2019-02-23 20:13:54 +0900

    ユーザー関連のコントローラー・DBの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :purchase_histories, dependent: :destroy
+  has_one :cart
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'products#index'
   devise_for :admins
   get "admins/top"
-  get "users/:id" => "users#show"
   get 'products/search'
   get 'products/show'
   get 'carts/index'
@@ -16,6 +15,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+resources :users
 resources :purchase_histories
 resources :admins
 
