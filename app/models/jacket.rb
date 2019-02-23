eea033381ee6f586e1bdb852b0d3d0518cb65cac
keitakamizawa <takamizawakei68@gmail.com>
2019-02-23 11:24:40 +0000

    モデルに親子関係追加。

--- app/models/jacket.rb
+++ app/models/jacket.rb
@@ -1,4 +1,5 @@
 class Jacket < ApplicationRecord
 
 attachment :image
+	belongs_to :product
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+	has_many :discs
+	has_many :jackets
+	has_many :purchase_contents
 end
