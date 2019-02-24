234a93469cf30f8798e4922fb3b46509e2eb94df
keitakamizawa <takamizawakei68@gmail.com>
2019-02-24 06:42:11 +0000

    cocoon関係箇所追記。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,10 +3,10 @@ class ProductsController < ApplicationController
   def index
   end
 
-# def index
-#   @q = Person.ransack(params[:q])
-#   @people = @q.result(distinct: true)
-# end
+    # def index
+    #   @q = Person.ransack(params[:q])
+    #   @people = @q.result(distinct: true)
+    # end
 
   def index
   	@q = Product.ransack(params[:q])
@@ -19,5 +19,16 @@ class ProductsController < ApplicationController
 
   def toppage
   end
+
+  def new
+    @oroduct = Product.new
+    @disc = @product.tasks.build
+    @song = @disc.songs.build
+  end
+
+  private
+    def product_params
+        params.require(:product).permit(:song_name, discs_attributes: [:id,])
+    end
 
 end
