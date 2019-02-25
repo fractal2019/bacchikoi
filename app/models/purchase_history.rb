7029f47207e387fc98345d7f0d4516d3d6a2876e
Takagi <uruhuodoneru2002@gmail.com>
2019-02-25 10:54:48 +0000

    エラー回避

--- app/models/purchase_history.rb
+++ app/models/purchase_history.rb
@@ -1,4 +1,4 @@
 class PurchaseHistory < ApplicationRecord
-	has_many: purchase_contents, dependent: :destroy
-	belongs_to: user
+	has_many :purchase_contents, dependent: :destroy
+	belongs_to :user
 end
