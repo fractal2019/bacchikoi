3da88f1a2762e327a880fd21ea4e1ede8631138b
kohei15 <101j00113@gmail.com>
2019-02-25 18:25:23 +0900

    バリデーションの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,27 +7,32 @@ class User < ApplicationRecord
   has_many :purchase_histories, dependent: :destroy
   has_one :cart
 
-  validates :name_kana, :format => {:with => /\A([ァ-ン]|ー)+\z/, :message =>'はカタカナで入力して下さい。'}
+  validates :name, presence: true, length: { minimum: 2, maximum: 30 }
+  validates :name_kana, presence: true, format: {with: /\A([ァ-ン]|ー)+\z/, :message =>'はカタカナで入力して下さい。'}
+  validates :postal_code, presence: true, format: {with: /\A\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}\z/, :message =>'は有効でありません。'}
+  validates :address, presence: true
+  validates :tel, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}-[0-9]{4}\z/, :message =>'は有効でありません。'}
 
-soft_deletable
 
-  # Deviseを使うと、問答無用でemailがユニーク扱いになる。
-  # それだと論理削除した際に再登録できないので、一旦emailに関する検証を削除する
-  # https://gist.github.com/brenes/4503386
-  _validators.delete(:email)
-  _validate_callbacks.each do |callback|
-    if callback.raw_filter.respond_to? :attributes
-      callback.raw_filter.attributes.delete :email
-    end
-  end
+# soft_deletable
 
-  # emailのバリデーションを定義し直す
-  validates :email, presence: true
-  validates_format_of :email, with: Devise.email_regexp, if: :email_changed?
-  validates_uniqueness_of :email, scope: :deleted_at, if: :email_changed?
+#   # Deviseを使うと、問答無用でemailがユニーク扱いになる。
+#   # それだと論理削除した際に再登録できないので、一旦emailに関する検証を削除する
+#   # https://gist.github.com/brenes/4503386
+#   _validators.delete(:email)
+#   _validate_callbacks.each do |callback|
+#     if callback.raw_filter.respond_to? :attributes
+#       callback.raw_filter.attributes.delete :email
+#     end
+#   end
 
-  def self.find_for_authentication(warden_conditions)
-    without_soft_destroyed.where(email: warden_conditions[:email]).first
-  end
+#   # emailのバリデーションを定義し直す
+#   validates :email, presence: true
+#   validates_format_of :email, with: Devise.email_regexp, if: :email_changed?
+#   validates_uniqueness_of :email, scope: :deleted_at, if: :email_changed?
+
+#   def self.find_for_authentication(warden_conditions)
+#     without_soft_destroyed.where(email: warden_conditions[:email]).first
+#   end
 
 end
