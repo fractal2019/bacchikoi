c9cde393b495017bbfa91f1d45302a9f6af862a5
kei <kotoura5545@gmail.com>
2019-02-26 11:35:46 +0000

    add db:enum

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,12 @@
      User.create(:name => "谷口#{i}", :name_kana => "タニグチ", :email => "email@gmail.com#{i}", :password => "00001#{i}", :address => "公園#{i}", :tel => "080-4545-545#{i}", :postal_code => "987-654#{i}")
      Label.create(:label_name => "ワーナーミュージックジャパン")
      Artist.create(artist_name: "ポルノグラフィティー#{i + 1}", label_id: "#{i}")
-     Product.create(price: 1000, number_of_stock: 50, product_introduction: "aaa",staff_comment: "bbb", artist_id: "#{i + 1}", product_name: "フラクタル#{i}")
+     Product.create(price: 1000, number_of_stock: 50, product_introduction: "aaa",staff_comment: "bbb", artist_id: "#{i + 1}", product_name: "フラクタル#{i}", type_name: "アルバム")
      Disc.create(disc_name: "disc#{i + 1}", product_id: "#{i + 1}")
-     Song.create(song_name: "曲#{i + 1}", disc_id: "#{i + 1}", genre_name: 1)
+     Song.create(song_name: "曲#{i + 1}", disc_id: "#{i + 1}", genre_name: "JーPOP")
      Cart.create(user_id: "#{i + 1}")
      CartContent.create(item_quantity: "#{i + 40}", carts_id: "#{i + 1}")
-     PurchaseHistory.create(user_id: "#{i + 1}", destination_postal_code: "080-454-545#{i}",destination_address: "公園#{i}")
+     PurchaseHistory.create(user_id: "#{i + 1}", destination_postal_code: "080-454-545#{i}",destination_address: "公園#{i}",delivery_status: "商品準備中")
      PurchaseContent.create(purchase_unit_price: "240#{i}", product_id: "#{i + 1}",item_quantity: "#{i}", purchase_history_id: "#{i + 1}")
 end
 2.times do |ad|
