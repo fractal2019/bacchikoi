e9df7ff3f609fe260b8ec8f258fdc014b2a11780
kei <kotoura5545@gmail.com>
2019-02-27 06:07:58 +0000

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,31 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # coding: utf-8
 
-9.times do |i|
-     User.create(:name => "谷口#{i}", :name_kana => "タニグチ", :email => "email@gmail.com#{i}", :password => "00001#{i}", :address => "公園#{i}", :tel => "080-4545-545#{i}", :postal_code => "987-654#{i}")
      Label.create(:label_name => "ワーナーミュージックジャパン")
-     Artist.create(artist_name: "ポルノグラフィティー#{i + 1}", label_id: "#{i}")
-     Product.create(price: 1000, number_of_stock: 50, product_introduction: "aaa",staff_comment: "bbb", artist_id: "#{i + 1}", product_name: "フラクタル#{i}", type_name: "アルバム")
-     Disc.create(disc_name: "disc#{i + 1}", product_id: "#{i + 1}")
-     Song.create(song_name: "曲#{i + 1}", disc_id: "#{i + 1}", genre_name: "JーPOP")
+     Artist.create(artist_name: "ポルノグラフィティー", label_id: "#{1}")
+2.times do |i|
+     User.create(:name => "谷口", :name_kana => "タニグチ", :email => "email@gmail.com#{i}", :password => "00001#{i}", :address => "公園#{i}", :tel => "080-4545-545#{i}", :postal_code => "987-654#{i}")
+     Product.create(price: 1000, number_of_stock: 50, product_introduction: "aaa",staff_comment: "bbb", artist_id: "#{1}", product_name: "フラクタル#{i}", type_name: "アルバム")
+end
+
+2.times do |i|
+     Disc.create(disc_name: "P1disc#{i + 1}", product_id: "#{1}")
+     Disc.create(disc_name: "disc#{i + 1}", product_id: "#{2}")
+     Song.create(song_name: "P1D1曲#{i + 1}", disc_id: "#{1}", genre_name: "JーPOP")
+     Song.create(song_name: "P1D2曲#{i + 1}", disc_id: "#{2}", genre_name: "KーPOP")
+     Song.create(song_name: "P2D1曲#{i + 1}", disc_id: "#{1}", genre_name: "洋楽")
+     Song.create(song_name: "P2D2曲#{i + 1}", disc_id: "#{2}", genre_name: "洋楽")
      Cart.create(user_id: "#{i + 1}")
-     CartContent.create(item_quantity: "#{i + 40}", carts_id: "#{i + 1}")
-     PurchaseHistory.create(user_id: "#{i + 1}", destination_postal_code: "080-454-545#{i}",destination_address: "公園#{i}",delivery_status: "商品準備中")
-     PurchaseContent.create(purchase_unit_price: "240#{i}", product_id: "#{i + 1}",item_quantity: "#{i}", purchase_history_id: "#{i + 1}")
+     CartContent.create(item_quantity: "#{i + 40}", carts_id: "#{1}")
+     CartContent.create(item_quantity: "#{i + 40}", carts_id: "#{2}")
+end
+2.times do |i|
+     PurchaseHistory.create(user_id: "#{1}", destination_postal_code: "080-454-545#{i}",destination_address: "公園#{i}",delivery_status: "商品準備中")
+     PurchaseHistory.create(user_id: "#{2}", destination_postal_code: "080-454-545#{i}",destination_address: "公園#{i}",delivery_status: "受付")
+     PurchaseContent.create(purchase_unit_price: "240#{i}", product_id: "#{1}",item_quantity: "#{i + 1}", purchase_history_id: "#{1}")
+     PurchaseContent.create(purchase_unit_price: "240#{i}", product_id: "#{1}",item_quantity: "#{i + 2}", purchase_history_id: "#{2}")
+     PurchaseContent.create(purchase_unit_price: "240#{i}", product_id: "#{2}",item_quantity: "#{i + 3}", purchase_history_id: "#{1}")
+     PurchaseContent.create(purchase_unit_price: "240#{i}", product_id: "#{2}",item_quantity: "#{i + 4}", purchase_history_id: "#{2}")
 end
 2.times do |ad|
 	Admin.create(email: "#{ad}", password: "#00000#{ad}")
