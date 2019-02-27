6aec91a282e73e447addecb6e51d682919ea00ef
kohei15 <101j00113@gmail.com>
2019-02-27 14:16:40 +0900

    購入日の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,9 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to roots
   end
 
   def edit
@@ -22,5 +25,8 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to root_path
   end
 end
