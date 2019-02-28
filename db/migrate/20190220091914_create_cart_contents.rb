98840a60e2dbdb7506d0396ffd49fa7c18913d2f
kohei15 <101j00113@gmail.com>
2019-02-28 12:30:57 +0900

    fix db

--- db/migrate/20190220091914_create_cart_contents.rb
+++ db/migrate/20190220091914_create_cart_contents.rb
@@ -1,6 +1,6 @@
 class CreateCartContents < ActiveRecord::Migration[5.2]
   def change
-    create_table :cart_content do |t|
+    create_table :cart_contents do |t|
       t.integer :item_quantity
       t.integer :cart_id
       t.integer :product_id
